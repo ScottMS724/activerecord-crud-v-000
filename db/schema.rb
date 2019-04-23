a6782adfc4e8afdfa601a1bb7f2ab560bb229e6e
Scott Silverman <scottms724@gmail.com>
2019-04-23 18:43:49 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 2019_04_23_174115) do
     t.integer "release_date"
     t.string "director"
     t.string "lead"
-    t.boolean "in_theaters"
+    t.boolean "in_theatres"
   end
 
 end

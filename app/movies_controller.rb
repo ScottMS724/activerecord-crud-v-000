a254a11750fe7dd2a4f1663a4413119e807cf03a
Scott Silverman <scottms724@gmail.com>
2019-04-23 18:17:34 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -47,7 +47,7 @@ def can_find_using_where_clause_and_be_sorted
       movies << movie 
     end 
   end 
-  movies.sort_by {|movie| movie.release_date|.reverse 
+  movies.sort_by {|movie| movie.release_date}.reverse 
 end
 
 def can_be_found_updated_and_saved
